036967b0e0abb9afd97c4080b0df71e74d62c9d0
Karina Gonzalez <karinagonzalez99@gmail.com>
2022-12-04 15:26:10 -0300

    Requisito 3

---
+++ src/components/Card.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Card extends Component {
+  render() {
+    const {
+      cardName,
+      cardDescription,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+      cardImage,
+      cardRare,
+      cardTrunfo,
+    } = this.props;
+    return (
+      <>
+        <span data-testid="name-card">{ cardName }</span>
+        <img src={ cardImage } alt={ cardName } data-testid="image-card" />
+        <span data-testid="description-card">{cardDescription}</span>
+        <span data-testid="attr1-card">{ cardAttr1 }</span>
+        <span data-testid="attr2-card">{ cardAttr2 }</span>
+        <span data-testid="attr3-card">{ cardAttr3 }</span>
+        <span data-testid="rare-card">{ cardRare }</span>
+        <p data-testid="trunfo-card">{ cardTrunfo === true ? 'Super Trunfo' : '' }</p>
+      </>
+    );
+  }
+}
+export default Card;
+Card.propTypes = {
+  cardName: PropTypes.string.isRequired,
+  cardDescription: PropTypes.string.isRequired,
+  cardAttr1: PropTypes.string.isRequired,
+  cardAttr2: PropTypes.string.isRequired,
+  cardAttr3: PropTypes.string.isRequired,
+  cardImage: PropTypes.string.isRequired,
+  cardRare: PropTypes.string.isRequired,
+  cardTrunfo: PropTypes.bool.isRequired,
+  // hasTrunfo: PropTypes.bool.isRequired,
+  // isSaveButtonDisabled: PropTypes.bool.isRequired,
+  // onInputChange: PropTypes.func.isRequired,
+  // onSaveButtonClick: PropTypes.func.isRequired,
+};
