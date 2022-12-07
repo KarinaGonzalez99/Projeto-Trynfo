68b0f152b698aabf6470c6625370618ba593cb81
Karina Gonzalez <karinagonzalez99@gmail.com>
2022-12-07 16:50:07 -0300

    Requisito 8

--- src/App.js
+++ src/App.js
@@ -107,6 +107,7 @@ class App extends React.Component {
       cardRare,
       cardTrunfo,
       isSaveButtonDisabled,
+      onSaveButtonClick,
     } = this.state;
     return (
       <div>
@@ -137,6 +138,22 @@ class App extends React.Component {
           cardTrunfo={ cardTrunfo }
           onSaveButtonClick={ this.onSaveButtonClick }
         />
+        <div className="Card">
+          { onSaveButtonClick.map((card) => (
+            <div key={ card.cardName }>
+              <Card
+                cardName={ card.cardName }
+                cardDescription={ card.cardDescription }
+                cardAttr1={ card.cardAttr1 }
+                cardAttr2={ card.cardAttr2 }
+                cardAttr3={ card.cardAttr3 }
+                cardImage={ card.cardImage }
+                cardRare={ card.cardRare }
+                cardTrunfo={ card.cardTrunfo }
+              />
+            </div>
+          ))}
+        </div>
       </div>
     );
   }
